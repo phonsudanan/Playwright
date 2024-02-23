c65bfde5ee7951c186ac092f4e0872e119ae8e1e
phonsudanan <phonsuda-nan@hotmail.com>
2024-02-23 14:13:28 +0700

    Edit step

--- tests/facebook.spec.js
+++ tests/facebook.spec.js
@@ -7,25 +7,24 @@ test('Post Facebook Hello Playwright!', async ({ page }) => {
         await expect(page.locator('h2')).toHaveText('Facebook ช่วยคุณเชื่อมต่อและแชร์กับผู้คนมากมายรอบตัวคุณ');
     });
     
-    await test.step('step 2 :: Input email', async ()  => {
+    await test.step('step 2 :: Input data', async ()  => {
+        // ใส่ E-mail
         const email = page.locator('#email');
         await expect(email).toBeEditable();
         await email.fill('');
-    });
-
-    await test.step('step 3 :: Input password', async ()  => {
+        // ใส่ Password
         const password = page.locator('id=pass');
         await expect(password).toBeEditable();
         await password.fill('');
     });
     
-    await test.step('step 4 :: Click login button', async ()  => {
+    await test.step('step 3 :: Click login button', async ()  => {
         const loginButton = page.getByTestId('royal_login_button');
         await expect(loginButton).toBeEnabled();
         await loginButton.click();
     });
     
-    await test.step('step 5 :: Post on facebook', async () => {
+    await test.step('step 4 :: Post on facebook', async () => {
         // ตรวจสอบ ชื่อ เมื่อเข้าสู่ระบบสำเร็จ
         await expect(page.getByText('Chloe Navia')).toBeVisible();
 
@@ -43,10 +42,9 @@ test('Post Facebook Hello Playwright!', async ({ page }) => {
         //คลิกปุ่ม โพสต์
         await expect(page.getByRole('button', { name: 'โพสต์' })).toBeEnabled();
         await page.getByRole('button', { name: 'โพสต์'}).click();
-        // await page.getByRole('button', { name: 'ปิด'}).click();
     });
 
-    await test.step('step 6 :: Verify "สวัสดี Playwright!" ', async () => {
+    await test.step('step 5 :: Verify "สวัสดี Playwright!" ', async () => {
         await expect(page.getByText('สวัสดี Playwright!')).toBeVisible();
     });
 
